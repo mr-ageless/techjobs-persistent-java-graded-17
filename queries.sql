a62fa9643c8b35bfb331c126a60a6c4ea6e4223f
Joe Minor <joeminor8533@gmail.com>
2023-11-08 12:31:30 -0600

    Final Commit V1.1

--- queries.sql
+++ queries.sql
@@ -7,7 +7,7 @@
 -- skills VARCHAR(255)
 
 --Part 2
-SELECT name FROM employers WHERE location = 'St. Louis City';
+SELECT name FROM employer WHERE location = "St. Louis City";
 --Part 3
 DROP TABLE job;
 --Part 4
